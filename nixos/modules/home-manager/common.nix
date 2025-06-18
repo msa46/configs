c4ed11c63a881cbbc9e7f3da9adc63119e25003a
Mohammad Sanaye Abbasi <mohamad.sanaye462@gmail.com>
2025-06-18 16:57:53 +0200

    Add common nix: Cofigs that should be the same accross all the systems.

---
+++ nixos/modules/home-manager/common.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  	programs.git = {
+    		enable = true;
+    		userName = "Mohammad Sanaye Abbasi";
+    		userEmail = "mohamad.sanaye462@gmail.com";
+  	};
+	  programs.nvf = {
+    		enable = true;
+    		# your settings need to go into the settings attribute set
+    		# most settings are documented in the appendix
+    		settings = {
+      			vim.viAlias = false;
+      			vim.vimAlias = true;
+      			vim.lsp = {
+        			enable = true;
+      			};
+	};
+  	};
+}
+
