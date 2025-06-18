e7edc741e129dfeffd95d3a6096ee2c54664cb20
Mohammad Sanaye Abbasi <mohamad.sanaye462@gmail.com>
2025-06-18 14:28:56 +0200

    Add home manager

--- nixos/laptop/flake.nix
+++ nixos/laptop/flake.nix
@@ -4,12 +4,17 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-      zen-browser = {
-    	url = "github:0xc000022070/zen-browser-flake";
-    	# IMPORTANT: we're using "libgbm" and is only available in unstable so ensure
-    	# to have it up-to-date or simply don't specify the nixpkgs input  
-    	inputs.nixpkgs.follows = "nixpkgs";
+      	zen-browser = {
+    		url = "github:0xc000022070/zen-browser-flake";
+    		# IMPORTANT: we're using "libgbm" and is only available in unstable so ensure
+    		# to have it up-to-date or simply don't specify the nixpkgs input  
+    		inputs.nixpkgs.follows = "nixpkgs";
   	};
+	home-manager = {
+		url = "github:nix-community/home-manager";
+    		inputs.nixpkgs.follows = "nixpkgs";
+	};
+	
   };
 
   outputs = inputs@{ self, nixpkgs, ... }: {
@@ -23,6 +28,15 @@
 		system = "x86_64-linux";
 		modules = [
 		./configuration.nix
+		home-manager.nixosModules.home-manager
+		{
+			home-manager.useGlobalPkgs = true;
+			home-manager.useUserPackages = true;
+			home-manager.users.jdoe = ../manager/home.nix;
+
+            		# Optionally, use home-manager.extraSpecialArgs to pass
+            		# arguments to home.nix
+		}
 		];	
 	};
     };
--- nixos/laptop/home.nix
+++ nixos/laptop/home.nix
@@ -1,41 +1,22 @@
-{ config, pkgs, system, inputs, ... }:
+{ config, pkgs, ... }:
 
 {
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
   home.username = "hq";
   home.homeDirectory = "/home/hq";
 
-  # Packages that should be installed to the user profile.
-  home.packages = with pkgs; [
-    zip
-    xz
-    unzip
-    p7zip
-    oh-my-zsh
-   oh-my-posh
-    inputs.zen-browser.packages."${system}".specific
-  ];
-
-  # basic configuration of git, please change to your own
-  programs.git = {
-    enable = true;
-    userName = "msa46";
-    userEmail = "mohamad.sanaye462@gmail.com";
-    extraConfig = {
-        init.defaultBranch = "main";
-    };
-  };
-
-
-  # This value determines the home Manager release that your
+  # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
+  # when a new Home Manager release introduces backwards
   # incompatible changes.
   #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
   # changes in each release.
-  #home.stateVersion = "23.11";
+  home.stateVersion = "25.05";
 
-  # Let home Manager install and manage itself.
-  #programs.home-manager.enable = true;
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
+
